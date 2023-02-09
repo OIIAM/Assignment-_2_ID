a1467ae75e10cec45023a7d856b67d62f4b08f81
Carleton <s10247735@connect.np.edu.sg>
2023-02-09 17:24:12 +0800

    added all js for btns

--- Main.js
+++ Main.js
@@ -7,21 +7,157 @@
 // })
 
 /* SOE */
-var modal = document.getElementById("Modals");
+var modalSOE = document.getElementById("SOEModals");
 
-var btn = document.getElementById("SOE");
+var btnSOE = document.getElementById("SOE");
 
-var span = document.getElementsByClassName("SOE-close")[0];
+var spanSOE = document.getElementsByClassName("SOE-close")[0];
 
-btn.onclick = function () {
-    modal.style.display = "block";
+btnSOE.onclick = function () {
+    modalSOE.style.display = "block";
 }
-span.onclick = function() {
-    modal.style.display = "none";
+spanSOE.onclick = function() {
+    modalSOE.style.display = "none";
 }
 
 window.onclick = function (event) {
-if (event.target == modal) {
-    modal.style.display = "none";
+if (event.target == modalSOE) {
+    modalSOE.style.display = "none";
+}
+}
+/* ICT */
+var modalICT = document.getElementById("ICTModals");
+
+var btnICT = document.getElementById("ICT");
+
+var spanICT = document.getElementsByClassName("ICT-close")[0];
+
+btnICT.onclick = function () {
+    modalICT.style.display = "block";
+}
+spanICT.onclick = function() {
+    modalICT.style.display = "none";
+}
+
+window.onclick = function (event) {
+if (event.target == modalICT) {
+    modalICT.style.display = "none";
+}
+}
+
+/* FMS */
+var modalFMS = document.getElementById("FMSM");
+
+var btnFMS = document.getElementById("FMS");
+
+var spanFMS = document.getElementsByClassName("FMS-close")[0];
+
+btnFMS.onclick = function () {
+    modalFMS.style.display = "block";
+}
+spanFMS.onclick = function() {
+    modalFMS.style.display = "none";
+}
+
+window.onclick = function (event) {
+if (event.target == modalFMS) {
+    modalFMS.style.display = "none";
+}
+}
+/* HMS */
+var modalHMS = document.getElementById("HMSM");
+
+var btnHMS = document.getElementById("HMS");
+
+var spanHMS = document.getElementsByClassName("HMS-close")[0];
+
+btnHMS.onclick = function () {
+    modalHMS.style.display = "block";
+}
+spanHMS.onclick = function() {
+    modalHMS.style.display = "none";
+}
+
+window.onclick = function (event) {
+if (event.target == modalHMS) {
+    modalHMS.style.display = "none";
+}
+}
+
+/* LSCT */
+var modalLSCT = document.getElementById("LSCTM");
+
+var btnLSCT = document.getElementById("LSCT");
+
+var spanLSCT = document.getElementsByClassName("LSCT-close")[0];
+
+btnLSCT.onclick = function () {
+    modalLSCT.style.display = "block";
+}
+spanLSCT.onclick = function() {
+    modalLSCT.style.display = "none";
+}
+
+window.onclick = function (event) {
+if (event.target == modalLSCT) {
+    modalLSCT.style.display = "none";
+}
+}
+/* BA */
+var modalBA = document.getElementById("BAM");
+
+var btnBA = document.getElementById("BA");
+
+var spanBA = document.getElementsByClassName("BA-close")[0];
+
+btnBA.onclick = function () {
+    modalBA.style.display = "block";
+}
+spanBA.onclick = function() {
+    modalBA.style.display = "none";
+}
+
+window.onclick = function (event) {
+if (event.target == modalBA) {
+    modalBA.style.display = "none";
+}
+}
+
+/* HS */
+var modalHS = document.getElementById("HSM");
+
+var btnHS = document.getElementById("HS");
+
+var spanHS = document.getElementsByClassName("HS-close")[0];
+
+btnHS.onclick = function () {
+    modalHS.style.display = "block";
+}
+spanHS.onclick = function() {
+    modalHS.style.display = "none";
+}
+
+window.onclick = function (event) {
+if (event.target == modalHS) {
+    modalHS.style.display = "none";
+}
+}
+/* DE */
+var modalDE = document.getElementById("DEM");
+
+var btnDE = document.getElementById("DE");
+
+var spanDE = document.getElementsByClassName("DE-close")[0];
+
+btnDE.onclick = function () {
+    modalDE.style.display = "block";
+}
+spanDE.onclick = function() {
+    modalDE.style.display = "none";
+}
+
+window.onclick = function (event) {
+if (event.target == modalDE) {
+    modalDE.style.display = "none";
 }
 }
